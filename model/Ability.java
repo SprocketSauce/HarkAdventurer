f224fa79a8b50358d5ee4b154be86d7504368f43
Jack McNair <18927430@student.curtin.edu.au>
2017-10-11 14:43:01 +0800

    Added some documentation

--- model/Ability.java
+++ model/Ability.java
@@ -5,6 +5,14 @@ import harkadventurer.control.TargetingStrategy;
 import harkadventurer.excep.AbilityException;
 import java.io.Serializable;
 
+/**
+ * <h1>Ability</h1>
+ * Ability-type objects store all information pertaining to an ability, including its
+ * name, base damage, dice, targeting and effect.
+ *
+ * @author Jack McNair 18927430
+ * @since 09/10/2017
+ */
 public class Ability implements Serializable
 {
 	// ===== CLASSFIELDS =====	
@@ -15,6 +23,10 @@ public class Ability implements Serializable
 	private int numDice;
 	private int typeDice;
 
+	/** 
+	 * Constructs an ability object with no name, effect and targeting, with a base 
+	 * damage of 0 and 0 2-sided dice.
+	 */
 	public Ability()
 	{
 		effect = null;
@@ -25,6 +37,17 @@ public class Ability implements Serializable
 		typeDice = 2;
 	} // end constructor
 	
+	/**
+	 * Constructs an ability object initialized with the given effect, name, target,
+	 * base effect and dice.
+	 * @param inEffect The ability's effect
+	 * @param inName The ability's name
+	 * @param inTarget The ability's targeting logic
+	 * @param inBase The number to be added to the ability's dice roll
+	 * @param inNumDice The number of dice to be rolled
+	 * @param inTypeDice The type of dice to be rolled
+	 * @throws AbilityException If any of the inputs are invalid
+	 */
 	public Ability( AbilityEffect inEffect, String inName, TargetingStrategy inTarget, int inBase, int inNumDice, int inTypeDice ) throws AbilityException
 	{
 		setEffect( inEffect );
@@ -35,30 +58,77 @@ public class Ability implements Serializable
 		setTypeDice( inTypeDice );
 	} // end constructor
 
-    // ===== ACCESSORS =====
-    public AbilityEffect getEffect() { return effect; }
-    public String getName() { return new String( name ); }
-    public TargetingStrategy getTarget() { return target; }
-    public int getBase() { return base; }
-    public int getNumDice() { return numDice; }
-    public int getTypeDice() { return typeDice; }
+	// ===== ACCESSORS =====
+
+	/** 
+	 * Returns the ability's effect. 
+	 * @return The ability's effect 
+	 */
+	public AbilityEffect getEffect() { return effect; }
+
+	/** 
+	 * Returns the ability's name. 
+	 * @return The ability's name
+	 */
+	public String getName() { return new String( name ); }
+
+	/** 
+	 * Returns the ability's targeting strategy. 
+	 * @return The ability's targeting strategy
+	 */
+	public TargetingStrategy getTarget() { return target; }
+
+	/** 
+	 * Returns the ability's base damage. 
+	 * @return The ability's base damage
+	 */
+	public int getBase() { return base; }
+
+	/** 
+	 * Returns the number of dice the ability rolls. 
+	 * @return The number of dice 
+	 */
+	public int getNumDice() { return numDice; }
+
+	/** 
+	 * Returns the number of faces on the dice the ability rolls. 
+	 * @return The dice type 
+	 */
+	public int getTypeDice() { return typeDice; }
     
     // ===== MUTATORS =====
+	/**
+	 * Sets the ability's effect to the input.
+	 * @param inEffect The desired effect
+	 */
 	public void setEffect( AbilityEffect inEffect )
 	{
 		effect = inEffect;
 	} // end method
-
+	
+	/**
+	 * Sets the ability's name.
+	 * @param inName The ability's name
+	 */
 	public void setName( String inName )
 	{
 		name = new String( inName );
 	} // end method
 
+	/**
+	 * Sets the ability's targeting logic.
+	 * @param inTarget The desired targeting logic
+	 */
 	public void setTarget( TargetingStrategy inTarget )
 	{
 		target = inTarget;
 	} // end method
 
+	/**
+	 * Sets the ability's base damage.
+	 * @param inBase The desired base damage
+	 * @throws AbilityException If the input is negative
+	 */
 	public void setBase( int inBase ) throws AbilityException
 	{
 		if ( inBase >= 0 )
@@ -71,6 +141,11 @@ public class Ability implements Serializable
 		}
 	} // end method
 
+	/**
+	 * Sets the number of dice to be rolled by the ability.
+	 * @param inNumDice The desired number of dice
+	 * @throws AbilityException If the input is negative
+	 */
 	public void setNumDice( int inNumDice ) throws AbilityException
 	{
 		if ( inNumDice >= 0 )
@@ -83,6 +158,12 @@ public class Ability implements Serializable
 		} // end if
 	} // end method
 
+	/**
+	 * Sets the type of dice to be rolled by the ability. A dice can have 2, 3, 4, 6, 8,
+	 * 10, 12, 20 or 100 faces.
+	 * @param inTypeDice The number of faces on the ability's dice
+	 * @throws AbilityException If the input is not a valid type of dice
+	 */
 	public void setTypeDice( int inTypeDice ) throws AbilityException
 	{
 		switch ( inTypeDice )
@@ -95,6 +176,10 @@ public class Ability implements Serializable
 		} // end switch
 	} // end method	
     
+	/**
+	 * Returns a String containing the ability's information.
+	 * @return A String containing all the ability's classfields.
+	 */
     public String toString()
     {
         String outStr;
--- model/Character.java
+++ model/Character.java
@@ -8,6 +8,13 @@ import harkadventurer.model.FriendlyObserver;
 import harkadventurer.model.EnemyObserver;
 import harkadventurer.excep.CharacterException;
 
+/**
+ * Character-type objects store a character's name, maximum health, current health and
+ * list of abilities.
+ *
+ * @author Jack McNair 18927430
+ * @since 09/10/2017
+ */
 abstract public class Character implements Targetable, Serializable
 {
 	// ===== CLASSFIELDS =====
@@ -17,64 +24,123 @@ abstract public class Character implements Targetable, Serializable
 	private LinkedList<Ability> abilities;
 	
 	// ----- Observers -----
-    private LinkedList<FriendlyObserver> fObservers;
-    private LinkedList<EnemyObserver> eObservers;
+	private LinkedList<FriendlyObserver> fObservers;
+	private LinkedList<EnemyObserver> eObservers;
 
-	//	=====CONSTRUCTORS=====
-	//	-----Default Constructor-----
+	// =====CONSTRUCTORS=====
+	// -----Default Constructor-----
+	/**
+	 * Constructs a character object with 1 hit point and no abilities or observers.
+	 */
 	public Character()
 	{
 		name = null;
 		maxHealth = 1;
 		curHealth = 1;
 		abilities = new LinkedList<Ability>();
-        fObservers = new LinkedList<FriendlyObserver>();
-        eObservers = new LinkedList<EnemyObserver>();
+		fObservers = new LinkedList<FriendlyObserver>();
+		eObservers = new LinkedList<EnemyObserver>();
 	} // end constructor
 
 	// -----Alternate Constructor-----
+	/**
+	 * Constructs a character object with the input name, hit points and abilities.
+	 * The character will have full health and no observers.
+	 *
+	 * @param inName The character's name
+	 * @param inMax The character's maximum hit points
+	 * @param inAbility A list of the character's abilities
+	 * @throws CharacterException If any of the inputs are invalid
+	 */
 	public Character( String inName, int inMax, LinkedList<Ability> inAbility ) throws CharacterException
 	{
 		setName( inName );
 		setMaxHealth( inMax );
 		setCurHealth( inMax );
 		abilities = inAbility;
+		fObservers = new LinkedList<FriendlyObserver>();
+		eObservers = new LinkedList<EnemyObserver>();
 	} // end constructor
 
 	// =====ACCESSORS=====
+	/**
+	 * Returns the character's name.
+	 * @return The character's name
+	 */
 	public String getName() { return new String( name ); }
+	
+	/**
+	 * Returns the character's maximum health.
+	 * @return The character's maximum health */
 	public int getMaxHealth() { return maxHealth; }
+
+	/**
+	 * Returns the character's current health.
+	 * @return The character's current health
+	 */
 	public int getCurHealth() { return curHealth; }
+
+	/**
+	 * Returns a list of the character's abilities.
+	 * @return A list of the character's abilities
+	 */
 	public LinkedList<Ability> getAbilities() { return (LinkedList<Ability>)abilities.clone(); }
 	
+	/**
+	 * Returns a list of the character's friendly observers.
+	 * @return a list of the character's friendly observers
+	 */
 	public LinkedList<FriendlyObserver> getFriendlyObservers() 
 	{ 
 		return (LinkedList<FriendlyObserver>)fObservers.clone(); 
 	} // end accessor
 	
+	/**
+	 * Returns a list of the character's enemy observers.
+	 * @return a list of the character's enemy observers
+	 */
 	public LinkedList<EnemyObserver> getEnemyObservers() 
 	{ 
 		return (LinkedList<EnemyObserver>)eObservers.clone(); 
 	} // end accessor
 
 	// =====MUTATORS=====
+	/**
+	 * Sets the character's name.
+	 * 
+	 * @param inName The desired name
+	 */
 	public void setName ( String inName )
 	{
 		name = new String( inName );
 	} // end mutator
 
+	/**
+	 * Sets the character's maximum health.
+	 * 
+	 * @param inMax The desired max healh
+	 * @throws CharacterException If the input is less than 20
+	 */
 	public void setMaxHealth( int inMax ) throws CharacterException
 	{
 		if ( inMax < 20 )
 		{
 			throw new CharacterException( "Max Health must be at least 20" );
-		}
+		}UI
 		else
 		{
 			maxHealth = inMax;
 		}
 	} // end mutator
 
+	/**
+	 * Sets the character's current health. If the input is higher than the
+	 * character's maximum health, current health will be set to maximum health.
+	 * If the input is 0 or less, the character's observers will be notified that
+	 * they have died.
+	 *
+	 * @param inCur The desired curren health
+	 */
 	public void setCurHealth( int inCur )
 	{
 		if ( inCur > maxHealth )
@@ -92,16 +158,30 @@ abstract public class Character implements Targetable, Serializable
 		}		
 	} // end mutator
 
+	/**
+	 * Changes the character's current health by the input amount. The character will
+	 * be healed if positive, damaged if negative.
+	 * 
+	 * @param inAmount The amount by which to change the charactet's health
+	 */
 	public void changeHealth( int amount )
 	{
 		setCurHealth( curHealth + amount );
 	} // end mutator
 
+	/**
+	 * Adds an ability to the character's list of abilities.
+	 * 
+	 * @param inAbility The ability to be added
+	 */
 	public void addAbility( Ability inAbility )
 	{
 		abilities.add( inAbility );
 	} // end mutator
 	
+	/**
+	 * Two characters are equal
+	 */
 	@Override
 	public boolean equals( Object inObj )
 	{
